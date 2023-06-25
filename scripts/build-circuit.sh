9f98e3aaa95e629515883e0cc6e6cfe6aef6d622
Alex E <alex@creatoros.co>
2023-06-26 01:08:07 +0500

    [FIX] optimized ptau and zkey generation

--- scripts/build-circuit.sh
+++ scripts/build-circuit.sh
@@ -5,13 +5,13 @@ circom circuits/circuit.circom --r1cs --wasm --O2 --inspect -o resources
 mv resources/circuit_js/circuit.wasm resources/circuit.wasm
 rm -rf resources/circuit_js
 echo "generating verification key..."
-snarkjs groth16 setup resources/circuit.r1cs pot/pot18_final.ptau resources/circuit_0000.zkey
-#snarkjs zkey contribute resources/circuit_0000.zkey resources/circuit_0001.zkey --name="1st Contributor" -v -e=$(openssl rand -hex 10240)
-#snarkjs zkey contribute resources/circuit_0001.zkey resources/circuit_0002.zkey --name="2nd Contributor" -v -e=$(openssl rand -hex 10240)
+npm exec snarkjs -- groth16 setup resources/circuit.r1cs pot/pot18_final.ptau resources/circuit_0000.zkey
+npm exec snarkjs -- zkey contribute resources/circuit_0000.zkey resources/circuit_0001.zkey --name="1st Contributor" -v -e=$(openssl rand -hex 10240)
+#npm exec snarkjs -- zkey contribute resources/circuit_0001.zkey resources/circuit_0002.zkey --name="2nd Contributor" -v -e=$(openssl rand -hex 10240)
 # last circuit_000x.zkey should go in here
-#snarkjs zkey beacon resources/circuit_0000.zkey resources/circuit_final.zkey $(curl https://drand.cloudflare.com/public/latest | jq -r ".randomness") 20 -n="Final Beacon phase2"
-snarkjs zkey beacon resources/circuit_0000.zkey resources/circuit_final.zkey $(openssl rand -hex 128) 20 -n="Final Beacon phase2"
-# snarkjs zkey verify resources/circuit.r1cs pot/pot18_final.ptau resources/circuit_final.zkey
+#npm exec snarkjs -- zkey beacon resources/circuit_0000.zkey resources/circuit_final.zkey $(curl https://drand.cloudflare.com/public/latest | jq -r ".randomness") 20
+npm exec snarkjs -- zkey beacon resources/circuit_0001.zkey resources/circuit_final.zkey $(openssl rand -hex 128) 20
+#npm exec snarkjs -- zkey verify resources/circuit.r1cs pot/pot18_final.ptau resources/circuit_final.zkey
 rm -rf resources/circuit_0000.zkey
 rm -rf resources/circuit_0001.zkey
 rm -rf resources/circuit_0002.zkey
--- scripts/generate-ptau.sh
+++ scripts/generate-ptau.sh
@@ -1,11 +1,18 @@
 set -e
-npm exec snarkjs -- powersoftau new bls12-381 18 pot18_0000.ptau -v
-npm exec snarkjs -- powersoftau contribute pot18_0000.ptau pot18_0001.ptau --name="First contribution" -v -e=$(openssl rand -hex 10240)
-npm exec snarkjs -- powersoftau contribute pot18_0001.ptau pot18_0002.ptau --name="Second contribution" -v -e=$(openssl rand -hex 10240)
-npm exec snarkjs -- powersoftau export challenge pot18_0002.ptau challenge_0003
-npm exec snarkjs -- powersoftau challenge contribute bls12-381 challenge_0003 response_0003 -e=$(openssl rand -hex 10240)
-npm exec snarkjs -- powersoftau import response pot18_0002.ptau response_0003 pot18_0003.ptau -n="Third contribution"
-npm exec snarkjs -- powersoftau verify pot18_0003.ptau
-npm exec snarkjs -- powersoftau beacon pot18_0003.ptau pot18_beacon.ptau $(curl https://beacon.nist.gov/beacon/2.0/pulse/last | jq -r ".pulse.outputValue") 10 -n="Final Beacon"
-npm exec snarkjs -- powersoftau prepare phase2 pot18_beacon.ptau pot18_final.ptau -v
-npm exec snarkjs -- powersoftau verify pot18_final.ptau
+npm exec snarkjs -- powersoftau new bls12-381 18 pot18_0000.ptau
+npm exec snarkjs -- powersoftau contribute pot18_0000.ptau pot18_0001.ptau --name="First contribution" -e=$(openssl rand -hex 10240)
+npm exec snarkjs -- powersoftau contribute pot18_0001.ptau pot18_0002.ptau --name="Second contribution" -e=$(openssl rand -hex 10240)
+npm exec snarkjs -- powersoftau contribute pot18_0002.ptau pot18_0003.ptau --name="Third contribution" -e=$(openssl rand -hex 10240)
+#npm exec snarkjs -- powersoftau verify pot18_0003.ptau
+npm exec snarkjs -- powersoftau beacon pot18_0003.ptau pot18_beacon.ptau $(openssl rand -hex 128) 20 -n="Final Beacon"
+#npm exec snarkjs -- powersoftau beacon pot18_0003.ptau pot18_beacon.ptau $(curl https://drand.cloudflare.com/public/latest | jq -r ".randomness") 20 -n="Final Beacon"
+npm exec snarkjs -- powersoftau prepare phase2 pot18_beacon.ptau pot18_final.ptau
+#npm exec snarkjs -- powersoftau verify pot18_final.ptau
+
+rm pot18_0000.ptau
+rm pot18_0001.ptau
+rm pot18_0002.ptau
+rm pot18_0003.ptau
+rm pot18_beacon.ptau
+mv pot18_final.ptau pot/pot18_final.ptau
+
