4338f8271afe02f7865a5a05e2cab09148e9587a
Adithya Krishna <aadithya794@gmail.com>
2024-08-04 18:50:19 +0530

    feat: improve ts files
    Signed-off-by: Adithya Krishna <aadithya794@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -1,48 +1,73 @@
-import { bitsToUint8Array, bitsToUintArray, toUint8Array, toUintArray, uint8ArrayToBits, uintArrayToBits } from "./utils"
+import { 
+    bitsToUint8Array, 
+    bitsToUintArray, 
+    toUint8Array, 
+    toUintArray, 
+    uint8ArrayToBits, 
+    uintArrayToBits 
+} from "./utils";
 
-export const CONFIG = {
-	'chacha20': {
-		chunkSize: 16,
-		bitsPerWord: 32,
-		keySizeBytes: 32,
-		ivSizeBytes: 12,
-		startCounter: 1,
-		// num of blocks per chunk
-		blocksPerChunk: 1,
-		// chacha20 circuit uses LE encoding
-		isLittleEndian: true,
-		uint8ArrayToBits: (arr: Uint8Array) => (
-			uintArrayToBits(toUintArray(arr))
-		),
-		bitsToUint8Array: (bits: number[]) => {
-			const arr = bitsToUintArray(bits)
-			return toUint8Array(arr)
-		},
-	},
-	'aes-256-ctr': {
-		chunkSize: 64,
-		bitsPerWord: 8,
-		keySizeBytes: 32,
-		ivSizeBytes: 12,
-		startCounter: 2,
-		// num of blocks per chunk
-		blocksPerChunk: 4,
-		// AES circuit uses BE encoding
-		isLittleEndian: false,
-		uint8ArrayToBits,
-		bitsToUint8Array,
-	},
-	'aes-128-ctr': {
-		chunkSize: 64,
-		bitsPerWord: 8,
-		keySizeBytes: 16,
-		ivSizeBytes: 12,
-		startCounter: 2,
-		// num of blocks per chunk
-		blocksPerChunk: 4,
-		// AES circuit uses BE encoding
-		isLittleEndian: false,
-		uint8ArrayToBits,
-		bitsToUint8Array,
-	}
+import { EncryptionAlgorithm } from "./types";
+
+/**
+ * Configuration for encryption algorithms
+ */
+export const CONFIG: Record<EncryptionAlgorithm, EncryptionConfig> = {
+    'chacha20': {
+        chunkSize: 16,
+        bitsPerWord: 32,
+        keySizeBytes: 32,
+        ivSizeBytes: 12,
+        startCounter: 1,
+        blocksPerChunk: 1,
+        isLittleEndian: true,
+        uint8ArrayToBits: (arr: Uint8Array) => uintArrayToBits(toUintArray(arr)),
+        bitsToUint8Array: (bits: number[]) => toUint8Array(bitsToUintArray(bits)),
+    },
+    'aes-256-ctr': {
+        chunkSize: 64,
+        bitsPerWord: 8,
+        keySizeBytes: 32,
+        ivSizeBytes: 12,
+        startCounter: 2,
+        blocksPerChunk: 4,
+        isLittleEndian: false,
+        uint8ArrayToBits: (arr: Uint8Array) => [uint8ArrayToBits(arr)],
+        bitsToUint8Array,
+    },
+    'aes-128-ctr': {
+        chunkSize: 64,
+        bitsPerWord: 8,
+        keySizeBytes: 16,
+        ivSizeBytes: 12,
+        startCounter: 2,
+        blocksPerChunk: 4,
+        isLittleEndian: false,
+        uint8ArrayToBits: (arr: Uint8Array) => [uint8ArrayToBits(arr)],
+        bitsToUint8Array,
+    }
+};
+
+/**
+ * Type definition for encryption configuration
+ */
+interface EncryptionConfig {
+    /** Size of each chunk in words */
+    chunkSize: number;
+    /** Number of bits in each word */
+    bitsPerWord: number;
+    /** Size of the encryption key in bytes */
+    keySizeBytes: number;
+    /** Size of the initialization vector in bytes */
+    ivSizeBytes: number;
+    /** Starting value for the counter */
+    startCounter: number;
+    /** Number of blocks processed in each chunk */
+    blocksPerChunk: number;
+    /** Whether the algorithm uses little-endian encoding */
+    isLittleEndian: boolean;
+    /** Function to convert Uint8Array to bits */
+    uint8ArrayToBits: (arr: Uint8Array) => number[][];
+    /** Function to convert bits to Uint8Array */
+    bitsToUint8Array: (bits: number[]) => Uint8Array;
 }
