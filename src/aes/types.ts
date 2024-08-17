6ce1fec205a8aad070543b5160c7803eff1413b9
Abdul Rashid <abdulrreshamwala@creatoros.co>
2024-08-17 16:50:34 +0000

    Revert "Merge branch 'feat/update' into 'master'"

--- src/aes/types.ts
+++ src/aes/types.ts
@@ -1,50 +1,32 @@
-/**
- * Represents a zero-knowledge proof
- */
-export interface Proof {
-    /** Serialized SnarkJS proof */
-    proofJson: string;
-    /** Decrypted plaintext */
-    plaintext: Uint8Array;
+
+export type Proof = {
+	/** serialised SnarkJS proof */
+	proofJson: string
+
+	plaintext: Uint8Array
 }
 
-/**
- * Represents a verification key for zero-knowledge proofs
- */
-export interface VerificationKey {
-    /** Binary data for .zkey file */
-    data: Uint8Array;
-    /** JSON representation of the verification key */
-    json?: unknown;
+export type VerificationKey = {
+	/** binary data for .zkey file */
+	data: Uint8Array
+	json?: any
 }
 
-/**
- * Represents a redaction range in the plaintext
- */
-export interface Redaction {
-    /** Starting index of the redaction */
-    startIndex: number;
-    /** Ending index of the redaction */
-    endIndex: number;
+export type Redaction = {
+	startIndex: number
+	endIndex: number
 }
 
-/**
- * Represents the private input for encryption
- */
-export interface PrivateInput {
-    /** AES-256-CTR key to decrypt ciphertext */
-    key: Uint8Array;
-    /** Initialization Vector (IV) for the ciphertext decryption */
-    iv: Uint8Array;
+export type PrivateInput = {
+	/** AES-256-CTR key to decrypt ciphertext */
+	key: Uint8Array
+	/** IV for the ciphertext decryption */
+	iv: Uint8Array
 }
 
-/**
- * Represents the public input for encryption
- */
-export interface PublicInput {
-    /** The ciphertext to decrypt */
-    ciphertext: Uint8Array;
-    // Commented out as per original file
-    // /** The redacted plaintext */
-    // redactedPlaintext: Uint8Array;
+export type PublicInput = {
+	/** the ciphertext to decrypt */
+	ciphertext: Uint8Array
+	/** the redacted plaintext */
+	//redactedPlaintext: Uint8Array
 }
--- src/gnark.ts
+++ src/gnark.ts
@@ -1,99 +1,142 @@
-import { EncryptionAlgorithm, ZKOperator, ZKProofInput } from "./types";
-import { join } from "path";
+import {EncryptionAlgorithm, ZKOperator} from "./types";
+import {join} from "path";
 
-let koffi: any;
-let verify: (...args: any[]) => any;
-let free: (...args: any[]) => any;
-let prove: (...args: any[]) => any;
+let koffi = require('koffi');
+
+
+let verify:(...args: any[]) => any
+let init:(...args: any[]) => any
+let free:(...args: any[]) => any
+let initComplete:(...args: any[]) => any
+let prove:(...args: any[]) => any
 
 try {
-    koffi = require('koffi');
-    if (koffi?.version) {
-        koffi.reset(); // Reset to avoid test failures
+	if(koffi?.version){
+		koffi.reset() //otherwise tests will fail
 
-        const GoSlice = koffi.struct('GoSlice', {
-            data: 'void *',
-            len: 'longlong',
-            cap: 'longlong'
-        });
+		// define object GoSlice to map to:
+		// C type struct { void *data; GoInt len; GoInt cap; }
+		const GoSlice = koffi.struct('GoSlice', {
+			data: 'void *',
+			len:  'longlong',
+			cap: 'longlong'
+		})
 
-        const ProveReturn = koffi.struct('ProveReturn', {
-            r0: 'void *',
-            r1: 'longlong',
-        });
+		const ProveReturn = koffi.struct('ProveReturn', {
+			r0: 'void *',
+			r1:  'longlong',
+		})
 
-        const resFolder = '../resources/gnark';
-        const arch = process.arch;
-        const libVerifyPath = join(__dirname, `${resFolder}/${arch}/libverify.so`);
-        const libProvePath = join(__dirname, `${resFolder}/${arch}/libprove.so`);
 
-        const libVerify = koffi.load(libVerifyPath);
-        const libProve = koffi.load(libProvePath);
+		const resFolder = `../resources/gnark`
 
-        verify = libVerify.func('Verify', 'unsigned char', [GoSlice]);
-        free = libProve.func('Free', 'void', ['void *']);
-        prove = libProve.func('Prove', ProveReturn, [GoSlice]);
-    }
-} catch (e) {
-    koffi = undefined;
-    console.log("Gnark is only supported on linux x64 & ARM64.", e.toString());
+		const arch = process.arch
+
+		const libVerifyPath = join(
+			__dirname,
+			`${resFolder}/${arch}/libverify.so`
+		)
+
+		const libProvePath = join(
+			__dirname,
+			`${resFolder}/${arch}/libprove.so`
+		)
+
+		const libVerify = koffi.load(libVerifyPath)
+		const libProve = koffi.load(libProvePath)
+
+		verify = libVerify.func('Verify', 'unsigned char', [GoSlice])
+		init = libProve.func('Init', 'void', [])
+		free = libProve.func('Free', 'void', ['void *'])
+		initComplete = libProve.func('InitComplete', 'unsigned char', [])
+		prove = libProve.func('Prove', ProveReturn, [GoSlice])
+	}
+} catch (e){
+	koffi = undefined
+	console.log("Gnark is only supported on linux x64 & ARM64.", e.toString())
 }
+
 
 export async function makeLocalGnarkZkOperator(cipher: EncryptionAlgorithm): Promise<ZKOperator> {
-    if (!koffi) {
-        return createUnsupportedOperator();
-    }
 
-    return {
-        generateWitness: async (input) => generateGnarkWitness(cipher, input),
-        groth16Prove: async (witness: Uint8Array) => {
-            const wtns = createGoSlice(witness);
-            const res = prove(wtns);
-            const resJson = Buffer.from(koffi.decode(res.r0, 'unsigned char', res.r1)).toString();
-            free(res.r0); // Avoid memory leak
-            return JSON.parse(resJson);
-        },
-        groth16Verify: async (publicSignals, proof) => {
-            const verifyParams = {
-                cipher,
-                proof: proof['proofJson'],
-                publicSignals,
-            };
-            const paramsBuf = strToUint8Array(JSON.stringify(verifyParams));
-            const params = createGoSlice(paramsBuf);
-            return verify(params) === 1;
-        },
-    };
+	if(koffi){
+		return Promise.resolve({
+
+			async generateWitness(input): Promise<Uint8Array> {
+				return generateGnarkWitness(cipher, input)
+			},
+
+			//used in nodeJS only for tests
+			async groth16Prove(witness: Uint8Array) {
+				const wtns = {
+					data: Buffer.from(witness),
+					len:witness.length,
+					cap:witness.length
+
+				}
+				const res = prove(wtns)
+				const resJson = Buffer.from(koffi.decode(res.r0, 'unsigned char', res.r1)).toString()
+				free(res.r0) // Avoid memory leak!
+
+				return Promise.resolve(JSON.parse(resJson))
+			},
+
+			async groth16Verify(publicSignals, proof) {
+				const proofStr = proof['proofJson']
+
+				const verifyParams = {
+					cipher:cipher,
+					proof: proofStr,
+					publicSignals: publicSignals,
+				}
+
+				const paramsJson = JSON.stringify(verifyParams)
+				const paramsBuf = strToUint8Array(paramsJson)
+
+				const params = {
+					data: paramsBuf,
+					len:paramsJson.length,
+					cap:paramsJson.length
+
+				}
+
+				return verify(params) === 1
+			},
+
+		})
+	} else {
+		return Promise.resolve({
+			async generateWitness(input): Promise<Uint8Array> {
+				return generateGnarkWitness(cipher, input)
+			},
+
+			async groth16Prove(witness) {
+				throw new Error("not supported")
+			},
+
+			async groth16Verify(publicSignals, proof) {
+				throw new Error("not supported")
+			},
+
+		})
+	}
 }
 
-function generateGnarkWitness(cipher: EncryptionAlgorithm, input: ZKProofInput): Uint8Array {
-    const proofParams = {
-        cipher,
-        key: input.key,
-        nonce: input.nonce,
-        counter: input.counter,
-        input: input.in,
-    };
-    return strToUint8Array(JSON.stringify(proofParams));
+function generateGnarkWitness(cipher, input){
+	//input is already in bits, sometimes groups of bits
+	const proofParams = {
+		cipher:cipher,
+		key: input.key,
+		nonce: input.nonce,
+		counter: input.counter,
+		input: input.in,
+	}
+
+	const paramsJson = JSON.stringify(proofParams)
+	return strToUint8Array(paramsJson)
 }
 
-function strToUint8Array(str: string): Uint8Array {
-    return new TextEncoder().encode(str);
+function strToUint8Array(str: string) {
+	return new TextEncoder().encode(str)
 }
 
-function createGoSlice(data: Uint8Array): any {
-    return {
-        data: Buffer.from(data),
-        len: data.length,
-        cap: data.length
-    };
-}
-
-function createUnsupportedOperator(): ZKOperator {
-    const unsupportedError = () => { throw new Error("not supported"); };
-    return {
-        generateWitness: async (input) => generateGnarkWitness('chacha20', input),
-        groth16Prove: unsupportedError,
-        groth16Verify: unsupportedError,
-    };
-}
