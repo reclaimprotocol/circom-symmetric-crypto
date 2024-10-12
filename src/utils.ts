ff61ebea676127e5c6b0abf6b3f92fd7f045b687
Srinath Murali <142419722+sensyble@users.noreply.github.com>
2024-10-13 01:37:38 +0530

    feat(utils): optimize bit manipulation functions

--- src/utils.ts
+++ src/utils.ts
@@ -1,171 +1,134 @@
-import { CONFIG } from "./config"
-import { EncryptionAlgorithm, UintArray } from "./types"
+import { CONFIG } from "./config";
+import { EncryptionAlgorithm, UintArray } from "./types";
 
-export const BITS_PER_WORD = 32
+export const BITS_PER_WORD = 32;
+export const REDACTION_CHAR_CODE = '*'.charCodeAt(0);
 
-// we use this to pad the ciphertext
-export const REDACTION_CHAR_CODE = '*'.charCodeAt(0)
-
-export function toUintArray(buf: Uint8Array) {
-	const arr = makeUintArray(buf.length / 4)
-	const arrView = new DataView(buf.buffer, buf.byteOffset, buf.byteLength)
-	for(let i = 0;i < arr.length;i++) {
-		arr[i] = arrView.getUint32(i * 4, true)
-	}
-	return arr
-}
-
-export function makeUintArray(init: number | number[]) {
-	return typeof init === 'number'
-		? new Uint32Array(init)
-		: Uint32Array.from(init)
+/**
+ * Converts a Uint8Array to a Uint32Array.
+ * Uses DataView for optimized memory access.
+ */
+export function toUintArray(buf: Uint8Array): UintArray {
+    const length = buf.length / 4;
+    const arr = new Uint32Array(length);
+    const view = new DataView(buf.buffer, buf.byteOffset, buf.byteLength);
+    for (let i = 0; i < length; i++) {
+        arr[i] = view.getUint32(i * 4, true);
+    }
+    return arr;
 }
 
 /**
- * Convert a UintArray (uint32array) to a Uint8Array
+ * Converts a Uint32Array to a Uint8Array.
  */
-export function toUint8Array(buf: UintArray) {
-	const arr = new Uint8Array(buf.length * 4)
-	const arrView = new DataView(arr.buffer, arr.byteOffset, arr.byteLength)
-	for(let i = 0;i < buf.length;i++) {
-		arrView.setUint32(i * 4, buf[i], true)
-	}
-	return arr
+export function toUint8Array(buf: UintArray): Uint8Array {
+    const arr = new Uint8Array(buf.length * 4);
+    const view = new DataView(arr.buffer);
+    for (let i = 0; i < buf.length; i++) {
+        view.setUint32(i * 4, buf[i], true);
+    }
+    return arr;
 }
 
-
+/**
+ * Pads a Uint8Array to ensure its length is a multiple of 4.
+ */
 export function padU8ToU32Array(buf: Uint8Array): Uint8Array {
+    const paddingLength = (4 - (buf.length % 4)) % 4;
+    if (paddingLength === 0) return buf;
 
-	if(buf.length % 4 === 0) {
-		return buf
-	}
-
-	return makeUint8Array(
-		[
-			...Array.from(buf),
-			...new Array(4 - buf.length % 4).fill(REDACTION_CHAR_CODE)
-		]
-	)
+    const paddedArray = new Uint8Array(buf.length + paddingLength);
+    paddedArray.set(buf);
+    paddedArray.fill(REDACTION_CHAR_CODE, buf.length);
+    return paddedArray;
 }
 
-export function makeUint8Array(init: number | number[]) {
-	return typeof init === 'number'
-		? new Uint8Array(init)
-		: Uint8Array.from(init)
-}
-
+/**
+ * Pads a UintArray to the specified size with REDACTION_CHAR_CODE.
+ */
 export function padArray(buf: UintArray, size: number): UintArray {
-	return makeUintArray(
-		[
-			...Array.from(buf),
-			...new Array(size - buf.length).fill(REDACTION_CHAR_CODE)
-		]
-	)
+    if (buf.length >= size) return buf;
+    const paddedArray = new Uint32Array(size);
+    paddedArray.set(buf);
+    paddedArray.fill(REDACTION_CHAR_CODE, buf.length);
+    return paddedArray;
 }
 
 /**
- * Converts a Uint8Array to an array of bits.
- * BE order.
+ * Converts a Uint8Array to an array of bits in BE order.
  */
-export function uint8ArrayToBits(buff: Uint8Array | number[]) {
-	const res: number[] = []
-	for (let i = 0; i < buff.length; i++) {
-		for (let j = 0; j < 8; j++) {
-			if ((buff[i] >> 7-j) & 1) {
-				res.push(1);
-			} else {
-				res.push(0);
-			}
-		}
-	}
-	return res;
+export function uint8ArrayToBits(buf: Uint8Array): number[] {
+    const bits: number[] = [];
+    for (const byte of buf) {
+        for (let i = 7; i >= 0; i--) {
+            bits.push((byte >> i) & 1);
+        }
+    }
+    return bits;
 }
 
 /**
- * Converts an array of bits to a Uint8Array.
- * Expecting BE order.
- * @param bits 
- * @returns 
+ * Converts an array of bits to a Uint8Array in BE order.
  */
-export function bitsToUint8Array(bits: number[]) {
-	const arr = new Uint8Array(bits.length / 8)
-	for(let i = 0;i < bits.length;i += 8) {
-		const uint = bitsToNum(bits.slice(i, i + 8))
-		arr[i / 8] = uint
-	}
-
-	return arr
+export function bitsToUint8Array(bits: number[]): Uint8Array {
+    const length = bits.length / 8;
+    const arr = new Uint8Array(length);
+    for (let i = 0; i < bits.length; i += 8) {
+        let byte = 0;
+        for (let j = 0; j < 8; j++) {
+            byte = (byte << 1) | bits[i + j];
+        }
+        arr[i / 8] = byte;
+    }
+    return arr;
 }
 
 /**
- * Converts a Uint32Array to an array of bits.
- * LE order.
+ * Converts a UintArray to an array of bits in LE order.
  */
-export function uintArrayToBits(uintArray: UintArray | number[]) {
-	const bits: number[][] = []
-	for (let i = 0; i < uintArray.length; i++) {
-		const uint = uintArray[i]
-		bits.push(numToBitsNumerical(uint))
-	}
-
-	return bits
-}
-
-export function bitsToUintArray(bits: number[]) {
-	const uintArray = new Uint32Array(bits.length / BITS_PER_WORD)
-	for(let i = 0;i < bits.length;i += BITS_PER_WORD) {
-		const uint = bitsToNum(bits.slice(i, i + BITS_PER_WORD))
-		uintArray[i / BITS_PER_WORD] = uint
-	}
-
-	return uintArray
-}
-
-function numToBitsNumerical(num: number, bitCount = BITS_PER_WORD) {
-	const bits: number[] = []
-	for(let i = 2 ** (bitCount - 1);i >= 1;i /= 2) {
-		const bit = num >= i ? 1 : 0
-		bits.push(bit)
-		num -= bit * i
-	}
-
-	return bits
-}
-
-function bitsToNum(bits: number[]) {
-	let num = 0
-
-	let exp = 2 ** (bits.length - 1)
-	for(let i = 0;i < bits.length;i++) {
-		num += bits[i] * exp
-		exp /= 2
-	}
-
-	return num
+export function uintArrayToBits(buf: UintArray): number[] {
+    const bits: number[] = [];
+    for (const uint of buf) {
+        for (let i = 0; i < BITS_PER_WORD; i++) {
+            bits.push((uint >> i) & 1);
+        }
+    }
+    return bits;
 }
 
 /**
- * Combines a 12 byte nonce with a 4 byte counter
- * to make a 16 byte IV.
+ * Converts an array of bits to a UintArray in LE order.
  */
-export function getFullCounterIv(nonce: Uint8Array, counter: number) {
-	const iv = Buffer.alloc(16)
-	iv.set(nonce, 0)
-	iv.writeUInt32BE(counter, 12)
-
-	return iv
+export function bitsToUintArray(bits: number[]): UintArray {
+    const length = bits.length / BITS_PER_WORD;
+    const arr = new Uint32Array(length);
+    for (let i = 0; i < bits.length; i += BITS_PER_WORD) {
+        let uint = 0;
+        for (let j = BITS_PER_WORD - 1; j >= 0; j--) {
+            uint = (uint << 1) | bits[i + j];
+        }
+        arr[i / BITS_PER_WORD] = uint;
+    }
+    return arr;
 }
 
 /**
- * Get the counter to use for a given chunk.
- * @param algorithm 
- * @param offsetInChunks 
- * @returns 
+ * Combines a 12-byte nonce with a 4-byte counter to form a 16-byte IV.
+ */
+export function getFullCounterIv(nonce: Uint8Array, counter: number): Buffer {
+    const iv = Buffer.alloc(16);
+    iv.set(nonce, 0);
+    iv.writeUInt32BE(counter, 12);
+    return iv;
+}
+
+/**
+ * Computes the counter value for a given chunk.
  */
 export function getCounterForChunk(
-	algorithm: EncryptionAlgorithm,
-	offsetInChunks: number
-) {
-	const { startCounter, blocksPerChunk } = CONFIG[algorithm]
-	return startCounter + offsetInChunks * blocksPerChunk
+    algorithm: EncryptionAlgorithm,
+    offsetInChunks: number
+): number {
+    const { startCounter, blocksPerChunk } = CONFIG[algorithm];
+    return startCounter + offsetInChunks * blocksPerChunk;
 }
